4dc44d16ca625b08e630d02086a0b19924f6b1ec
Westin Newell <wes.newell@starmakerinteractive.com>
2012-02-11 13:51:23 -0800

    Initial Commit

---
+++ RoboDJ/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RoboDJ
+//
+//  Created by Westin Newell on 2/11/12.
+//  Copyright (c) 2012 n8chur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RoboDJ/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RoboDJ
+//
+//  Created by Westin Newell on 2/11/12.
+//  Copyright (c) 2012 n8chur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
