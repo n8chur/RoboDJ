6a74d2da44fd819b92eaca61257518ba77b9da7f
Micha Mazaheri <micha@mazaheri.me>
2012-02-11 16:19:53 -0800

    Fixed library linking.

--- RoboDJ/SpotifyViewController.m
+++ RoboDJ/SpotifyViewController.m
@@ -43,8 +43,15 @@
 	};
 	const size_t g_appkey_size = sizeof(g_appkey);
 	
-//	NSData *appKey = [NSData dataWithBytes:g_appkey length:g_appkey_size];
-//	[SPSession initializeSharedSessionWithApplicationKey:appKey userAgent:@"com.westinnewell.RobotDJ" error:NULL];
+	NSData *appKey = [NSData dataWithBytes:g_appkey length:g_appkey_size];
+	NSError *error = NULL;
+	[SPSession initializeSharedSessionWithApplicationKey:appKey userAgent:@"com.westinnewell.RobotDJ" error:&error];
+	if (error != NULL) {
+		NSLog(@"Error: %@", [error localizedDescription]);
+	}
+	
+	SPTrack *track = [SPTrack trackForTrackURL:[NSURL URLWithString:@"http://open.spotify.com/track/22WbYJtuWEHpNmTJnYGpIw"] inSession:[SPSession sharedSession]];
+	NSLog(@"track: %@", track);
 }
 
 - (void)viewDidUnload
