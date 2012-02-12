1d74d015530433b56faf45be67fa8bd10aba4d4e
Westin Newell <wes.newell@starmakerinteractive.com>
2012-02-12 09:45:26 -0800

    Login working in new storyboard.

---
+++ RoboDJ/ContributeTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  ContributeViewController.h
+//  RoboDJ
+//
+//  Created by Westin Newell on 2/12/12.
+//  Copyright (c) 2012 n8chur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContributeViewController : UITableViewController
+
+
+
+@end
---
+++ RoboDJ/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  RoboDJ
+//
+//  Created by Westin Newell on 2/12/12.
+//  Copyright (c) 2012 n8chur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
